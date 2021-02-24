2c1998ec9fd870f27ff09672d08d193f46a19e3d
sjasminetya <shaulajasminetya22@gmail.com>
2021-02-24 17:54:11 +0700

    add product list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
     <Header />
+    <div>
+      <ProductList />
+    </div>
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import ProductList from './components/ProductList'
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    ProductList
   }
 }
 </script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container">
+    <div class="row">
+        <div class="card mt-5 ml-3" style="width: 16rem;" v-for="item in products" :key="item.id">
+            <img :src="item.imgUrl" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">{{item.title}}</h5>
+                <p class="card-text">{{item.price}}</p>
+                <a href="#" class="btn btn-primary">Add to cart</a>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import products from '../mock/products.json'
+export default {
+  name: 'ProductList',
+  data () {
+    return {
+      products
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
