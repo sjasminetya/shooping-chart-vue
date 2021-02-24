f5adc0c7c7109c5dfc8050f1eeed64560fa20f74
sjasminetya <shaulajasminetya22@gmail.com>
2021-02-24 17:34:56 +0700

    add component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
+
+<script>
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
 
 <style lang="scss">
 
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav class="navbar navbar-light bg-light">
+            <span class="navbar-brand mb-0 h1">Navbar</span>
+            <span class="navbar-brand mb-0 h1">Cart (0)</span>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+
+</style>
